0f1cafea1f808ac6da035de3a37d00f1601d4999
Jason Bube <jasonbube@dyn-10-108-12-152.wless.uoregon.edu>
2024-04-03 14:17:54 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    public TextMeshProUGUI countText;
+
+    private int count;
+
+    public GameObject winTextObject;
+
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+    public int jumpCount;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && (jumpCount <= 1))
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            jumpCount++;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 6)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision floor)
+    {
+        if (floor.gameObject.tag.Equals("Ground") == true)
+        {
+            jumpCount = 0;
+        }
+    }
+}
